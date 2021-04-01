e0b3030c47ded9b1acf99811229e1cbae9427a76
veblenzhang <veblenzhang@163.com>
2021-04-01 14:49:19 +0800

    Update redis-upgrade.sh

--- src/etcdkeeper/redis-upgrade.sh
+++ src/etcdkeeper/redis-upgrade.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 export ETCDCTL_API=2
 etcd_endpoints="http://"
@@ -9,13 +9,13 @@ declare -a finish_machines
 
 function log() {
     local ts=`date "+%F %T"`
-    echo  "$ts $*" 
+    echo  "$ts $*"
 }
 
 function log_c() {
     local ts=`date "+%F %T"`
     echo -n "$ts "
-    echo -e "\033[1;32m$*\033[0m" 
+    echo -e "\033[1;32m$*\033[0m"
 }
 
 function error() {
@@ -29,14 +29,13 @@ function error() {
         log "完成列表:"
         log ${finish_machines[*]}
     fi
-
-
 }
 
 
 function write_memory() {
 	echo -n "$1:$2 " >> ${path}/memory.list
-	used=`./redis-cli -h $1 -p $2 $pwd info memory | grep used_memory: | awk -F ":" '{print $2}' | tr -d "\r"` 
+    purge=`./redis-cli -h $1 -p $2 $pwd memory purge`
+	used=`./redis-cli -h $1 -p $2 $pwd info memory | grep used_memory_rss: | awk -F ":" '{print $2}' | tr -d "\r"`
 	echo -n "$used " >> ${path}/memory.list
 	maxmemory=`./redis-cli -h $1 -p $2 $pwd config get maxmemory | grep -v "maxmemory"`
 	echo -n "$maxmemory" >> ${path}/memory.list
@@ -46,7 +45,7 @@ function write_memory() {
 function list_machine_node() {
     log_c "------------------------------------"
     log "当前集群的主从分布情况："
-    ./redis-cli -c -h $1 -p $2  $pwd cluster nodes | awk  -F: '{print $1 " " $2}' | awk  ' {if ($4 ~ /master/){freq[$2, "master"]++; machine[$2]++} else if($4 ~ /slave/){freq [$2,"slave"]++;machine[$2]++} else{freq [$2, $4]++;machine[$2]++} } END{printf("%-15s %8s %8s \n" ,"machine", "master", "slave"); for (var in machine) { if (freq[var, "master"] !=  freq[var, "slave"]) {printf("\033[1;31m%-15s %8.2f %8.2f \033[0m\n", var, freq[var, "master"], freq[var, "slave"])} else { printf("\033[1;32m%-15s %8.2f %8.2f \033[0m\n", var, freq[var, "master"], freq[var, "slave"])}}}' 
+    ./redis-cli -c -h $1 -p $2  $pwd cluster nodes | awk  -F: '{print $1 " " $2}' | awk  ' {if ($4 ~ /master/){freq[$2, "master"]++; machine[$2]++} else if($4 ~ /slave/){freq [$2,"slave"]++;machine[$2]++} else{freq [$2, $4]++;machine[$2]++} } END{printf("%-15s %8s %8s \n" ,"machine", "master", "slave"); for (var in machine) { if (freq[var, "master"] !=  freq[var, "slave"]) {printf("\033[1;31m%-15s %8.2f %8.2f \033[0m\n", var, freq[var, "master"], freq[var, "slave"])} else { printf("\033[1;32m%-15s %8.2f %8.2f \033[0m\n", var, freq[var, "master"], freq[var, "slave"])}}}'
     ./redis-cli -c -h $1 -p $2  $pwd cluster nodes | awk  -F: '{print $1 " " $2}' | awk  ' {if ($4 ~ /master/){freq[$2, "master"]++; machine[$2]++} else if($4 ~ /slave/){freq [$2,"slave"]++;machine[$2]++} else{freq [$2, $4]++;machine[$2]++} } END{printf("%-15s %8s %8s \n" ,"machine", "master", "slave"); for (var in machine) { if (freq[var, "master"] !=  freq[var, "slave"]) {printf("%-15s %8.2f %8.2f \n", var, freq[var, "master"], freq[var, "slave"])} else { printf("%-15s %8.2f %8.2f \n", var, freq[var, "master"], freq[var, "slave"])}}}' > ${path}/machine.list
 }
 
@@ -59,13 +58,20 @@ function check_node() {
     if [ "${PONG}"x != PONGx ]; then
         error "无法连接 "$1":"$2
         exit 1
-    fi 
-    log "连通性 OK..."   
+    fi
+    log "连通性 OK..."
 
     log "主从节点分布..."
+
+    m_size=`./redis-cli -h $1 -p $2 $pwd cluster nodes | grep -v disconnected | grep master | wc -l`
+    s_size=`./redis-cli -h $1 -p $2 $pwd cluster nodes | grep -v disconnected | grep slave | wc -l`
+    if [ ${s_size} -ne ${m_size} ]; then
+        error "检查失败，主从节点不一致"
+        exit 1
+    fi
+
     # 红色表示master和slave在同一台主机上
-    ./redis-cli -c -h $1 -p $2  $pwd  cluster nodes | awk '{if ($4 != "-" ){$1 = $4} hash[$1]++; if($3 ~ /master/){ split($2, masternode, "@") ; freq[$1, "master"]=masternode[1]; hashSlot[$1] = $9} else if($3 ~ /slave/){split($2, slavenode, "@") ; freq[$1, "slave"]=slavenode[1] } else{freq [$1, $3]=$2}} END{printf("%-40s %-20s %-20s %-20s\n" ,"hashCode", "master", "slave", "hashSlot"); for (var in hash) {split(freq[var, "master"], master, ":"); master_ip = master[1] ;  split(freq[var, "slave"], slave, ":") ;slave_ip = slave[1] ;  if (master_ip == slave_ip) { printf("\033[1;31m%-40s %-20s %-20s %-20s \033[0m\n", var, freq[var, "master"], freq[var, "slave"], hashSlot[var]) } else if (slave_ip == '') { printf("\033[1;31m%-40s %-20s %-20s %-20s \033[0m\n", var, freq[var, "master"], freq[var, "slave"], hashSlot[var]) } else {  printf("\033[1;32m%-40s %-20s %-20s %-20s \033[0m\n", var, freq[var, "master"], freq[var, "slave"], hashSlot[var])}}}'
-    ./redis-cli -c -h 10.31.52.18 -p 2883 -a AccsService@123  cluster nodes | awk '{if ($4 != "-" ){$1 = $4} hash[$1]++; if($3 ~ /master/){ split($2, masternode, "@") ; freq[$1, "master"]=masternode[1]; hashSlot[$1] = $9} else if($3 ~ /slave/){split($2, slavenode, "@") ; freq[$1, "slave"]=slavenode[1] } else{freq [$1, $3]=$2}} END{printf("%-40s %-20s %-20s %-20s\n" ,"hashCode", "master", "slave", "hashSlot"); for (var in hash) {split(freq[var, "master"], master, ":"); master_ip = master[1] ;  split(freq[var, "slave"], slave, ":") ;slave_ip = slave[1] ;  if (master_ip == slave_ip) { printf("\033[1;31m%-40s %-20s %-20s %-20s \033[0m\n", var, freq[var, "master"], freq[var, "slave"], hashSlot[var]) } else if (slave_ip == '') { printf("\033[1;31m%-40s %-20s %-20s %-20s \033[0m\n", var, freq[var, "master"], freq[var, "slave"], hashSlot[var]) } else {  printf("\033[1;32m%-40s %-20s %-20s %-20s \033[0m\n", var, freq[var, "master"], freq[var, "slave"], hashSlot[var])}}}'
+    ./redis-cli -c -h $1 -p $2  $pwd  cluster nodes | awk '{if ($4 != "-" ){$1 = $4} hash[$1]++; if($3 ~ /master/){ split($2, masternode, "@") ; freq[$1, "master"]=masternode[1]; hashSlot[$1] = $9} else if($3 ~ /slave/){split($2, slavenode, "@") ; freq[$1, "slave"]=slavenode[1] } else{freq [$1, $3]=$2}} END{printf("%-40s %-20s %-20s %-20s\n" ,"hashCode", "master", "slave", "hashSlot"); for (var in hash) {split(freq[var, "master"], master, ":"); master_ip = master[1] ;  split(freq[var, "slave"], slave, ":") ;slave_ip = slave[1] ;  if (master_ip == slave_ip  || null == slave_ip ) { printf("\033[1;31m%-40s %-20s %-20s %-20s \033[0m\n", var, freq[var, "master"], freq[var, "slave"], hashSlot[var]) } else {  printf("\033[1;32m%-40s %-20s %-20s %-20s \033[0m\n", var, freq[var, "master"], freq[var, "slave"], hashSlot[var])}}}'> ${path}/cluster_node.info
     check_count=`cat ${path}/cluster_node.info | grep "1;31m" | wc -l`
     if [ ${check_count} -ne 0 ]; then
         error "检查失败，存在一主从在同一主机上："
@@ -102,7 +108,7 @@ function check_node() {
         maxmeory=${arr[2]}
         rate=`expr $used '*' 100 '/' $maxmeory`
         if [ $rate -gt 60 ]
-        then 
+        then
             error "${arr[0]} 内存校验失败 , 当前内存： ${rate}%"
             exit 1
         fi
@@ -117,12 +123,12 @@ function save_node() {
     str=(`cat ${path}/cluster_node.info | awk 'NR>1 {printf ("%s,%s\n"), $2,$3}'`)
 
     # 保存原始信息
-    for i in ${!str[@]} 
+    for i in ${!str[@]}
     do
         # 赋值
         eval $(echo ${str[i]} | awk '{split($0, filearray, ",");print "node_map["filearray[1]"]="filearray[2]}')
         eval $(echo ${str[i]} | awk '{split($0, filearray, ",");print "slave_node_map["filearray[2]"]="filearray[1]}')
-    done 
+    done
 
     machine_list=($(cat ${path}/machine.list | awk 'NR>1 {print $1}'))
     log "主机列表："
@@ -137,7 +143,7 @@ function save_node() {
             error "SSH [dcs@${machine}] has no access!"
             exit 1
         fi
-    done   
+    done
 
     # 检测主机sudo权限
     for machine in ${machine_list[@]}
@@ -149,7 +155,7 @@ function save_node() {
         else
             log "${machine} sudo test ok"
         fi
-    done   
+    done
 }
 
 function get_nodes() {
@@ -169,8 +175,8 @@ function check_switch() {
     if [ "${PONG}"x != PONGx ]; then
         error "无法连接 "$1":"$2
         exit 1
-    fi 
-    log "$1:$2 连通性 OK..."   
+    fi
+    log "$1:$2 连通性 OK..."
 
     size=${#node_map[@]}
     slave_size=${#slave_node_map[@]}
@@ -201,6 +207,10 @@ function check_switch() {
         log "$1:$2 集群间节点配置信息校验..."
         rm -f ${path}/*.config
         ./redis-cli -h $1 -p $2 $pwd cluster nodes | awk '{print $2}' | awk -F '@' '{print $1}' | sed 's/:/ /g' | while read ip port;do ./redis-cli -h $ip -p $port $pwd cluster nodes | sed 's/myself,//g' | sort | awk '{$4=null;$5=null;$6=null;$7=null;print $0}' > ${path}/${ip}_${port}.config;done
+        if ! ls ${path}/*.config 1> /dev/null 2>&1; then
+            continue
+        fi
+
         config_count_tmp=`ls ${path}/*.config | grep -v total | awk '{print $NF}' | xargs sha256sum | awk '{print $1}' | sort | uniq -c | wc -l`
         if [ ${config_count_tmp} -eq 1 ]; then
             log "$1:$2 集群间节点配置信息 OK..."
@@ -211,11 +221,11 @@ function check_switch() {
 }
 
 function check() {
-    if [ ${dc_name}x == ''x ]; then 
+    if [ ${dc_name}x == ''x ]; then
         log 'dc_name cant be empty'
         exit 1
     fi
-    if [ ${cluster_name}x == ''x ]; then 
+    if [ ${cluster_name}x == ''x ]; then
         log 'cluster_name cant be empty'
         exit 1
     fi
@@ -226,20 +236,19 @@ function init_path() {
     path=${current_path}/${dc_name}/${cluster_name}
     if [ ! -d "${path}" ]; then
         mkdir -p ${path}
-    else 
+    else
         rm -rf ${path}/*
     fi
-
 }
 
 function fail_over() {
     log "$3:$4 -> slave | $1:$2 -> master  | 准备"
     # 进行failover
     result=`./redis-cli -c -h $1 -p $2  $pwd cluster failover`
-    if [ "${result}" != "OK" ] ;then
-        error "$3:$4 -> slave | $1:$2 -> master  | 失败 | ${result}"
-        exit 1
-    fi
+    #if [ "${result}" != "OK" ] ;then
+    #    error "$3:$4 -> slave | $1:$2 -> master  | 失败 | ${result}"
+    #    exit 1
+    #fi
 
     let count=0
     # 检查failover是否完成
@@ -260,52 +269,135 @@ function fail_over() {
         log "$3:$4 正在切换...."
         let count+=1
         # 强制进行切换
-        if [ "$(($count % 10))" = "4" ]; then
-            log "$3:$4 强制切换...."
+        if [ "$(($count % 10))" = "0" ]; then
+            log "$3:$4 重试切换...."
             fail_over_force $1 $2 $3 $4
         fi
         sleep 2
-    done    
+    done
     log_c "$1:$2 切换成功"
 }
 
 function fail_over_force() {
     # 进行failover
-    result=`./redis-cli -c -h $1 -p $2  $pwd cluster failover FORCE`
+    # result=`./redis-cli -c -h $1 -p $2  $pwd cluster failover FORCE`
+    result=`./redis-cli -c -h $1 -p $2  $pwd cluster failover`
     if [ "${result}" != "OK" ] ;then
         error "$3:$4 -> slave | $1:$2 -> master  | 失败"
         exit 1
-    fi 
+    fi
 }
 
 function restart_machine() {
     log "开始重启 : $1"
     ssh -o "StrictHostKeyChecking no" dcs@$1 "sudo reboot 2 >/dev/null 2>&1 &"
     log "$1 正在重启中... "
-    sleep 20
+    sleep 120
     while true
     do
         PONG=`./redis-cli -c -h $1 -p $2  $pwd ping 2>/dev/null`
         if [ "${PONG}"x == PONGx ]; then
             break
-        fi 
+        fi
         log "$1 正在重启中... "
-        sleep 5
+        sleep 30
     done
+    sleep 120
     log "$1 重启完成... "
 }
 
-function check_slave_status() { 
+function check_slave_status() {
+    log "开始检查从节点状态 : $1:$2"
+    ./redis-cli -c -h $1 -p $2  $pwd info Replication | grep "role:slave"
+    if [[ $? == 1 ]] ; then
+        # 集群中间发生过主从切换，不要停止，在此基础上继续进行操作
+        log "$1:$2 非从节点，切换失败，尝试继续"
+        check_master_status $1 $2
+        return 0
+    fi
+
     while true
-    do 
+    do
+        ./redis-cli -c -h $1 -p $2  $pwd info Replication | grep "master_sync_in_progress:0"
+
+        if [[ $? == 0 ]] ; then
+            break
+        fi
+        log "$1:$2 从节点正在等待主节点数据......"
+        sleep 5
+    done
+
+    while true
+    do
+        ./redis-cli -c -h $1 -p $2  $pwd info Replication | grep "master_link_status:up"
+
+        if [[ $? == 0 ]] ; then
+            break
+        fi
+        log "$1:$2 从节点正在接受主节点数据......"
+        sleep 5
+    done
+
+    while true
+    do
         # loadin不为0说明当前节点不可用
         ./redis-cli -c -h $1 -p $2  $pwd info persistence | grep "loading:0"
 
         if [[ $? == 0 ]] ; then
             break
         fi
-        log "$1:$2节点正在初始化......"
+        log "$1:$2 从节点正在加载数据......"
         sleep 2
+    done
+
+
+    while true
+    do
+        # 检测offset是否正确
+        ./redis-cli -c -h $1 -p $2  $pwd info Replication | grep "master_repl_offset:0$"
+
+        if [[ $? != 0 ]] ; then
+            ./redis-cli -c -h $1 -p $2  $pwd info Replication | grep "slave_repl_offset:1$"
+            if [[ $? != 0 ]] ; then
+                break
+            fi
+        fi
+        log "$1:$2 从节点正在加载数据......"
+        sleep 2
+    done
+}
+
+function check_master_status() {
+    log "开始检查主节点节点状态 : $1:$2"
+    ./redis-cli -c -h $1 -p $2  $pwd info Replication | grep "role:master"
+    if [[ $? == 1 ]] ; then
+        # 非主节点，中间发生了切换
+        # 集群中间发生过主从切换，不要停止，在此基础上继续进行操作
+        log "$1:$2 非主节点，切换失败，尝试继续"
+        check_slave_status  $1 $2
+        return 0
+    fi
+    while true
+    do
+        # loadin不为0说明当前节点不可用
+        ./redis-cli -c -h $1 -p $2  $pwd info persistence | grep "rdb_bgsave_in_progress:0"
+
+        if [[ $? == 0 ]] ; then
+            break
+        fi
+        log "$1:$2 主节点数据正在Dump......"
+        sleep 5
+    done
+
+    while true
+    do
+        ./redis-cli -c -h $1 -p $2  $pwd info Replication | grep "state=online"
+
+        if [[ $? == 0 ]] ; then
+            break
+        fi
+        log "$1:$2 等待从节点上线......"
+        sleep 5
     done
 }
 
@@ -324,25 +416,42 @@ function restart() {
         # 针对新的集群信息进行解析
         str_new=(`cat ${path}/cluster_node_2.info | awk 'NR>1 {printf ("%s,%s\n"), $2,$3}'`)
         log ${str_new[*]}
-        for i in ${!str_new[@]} 
+        for i in ${!str_new[@]}
         do
             # 赋值
             eval $(echo ${str_new[i]} | awk '{split($0, filearray_new, ",");print "node_map_new["filearray_new[1]"]="filearray_new[2]}')
-            # eval $(echo ${str_new[i]} | awk '{split($0, filearray_new, ",");print "slave_node_map_new["filearray_new[2]"]="filearray_new[1]}')
-        done 
-        
+            eval $(echo ${str_new[i]} | awk '{split($0, filearray_new, ",");print "slave_node_map_new["filearray_new[2]"]="filearray_new[1]}')
+        done
+
+        for key in ${!slave_node_map_new[@]}
+        do
+                slave_node=(${key//:/ })
+                if [[ ${slave_node[0]} == ${machine} ]]
+                then
+                    node_port=${slave_node[1]}
+                    check_slave_status ${machine} ${node_port}
+                fi
+        done
+
         switch=false
         for key in ${!node_map_new[@]}
-        do  
-            master_node=(${key//:/ }) 
+        do
+            master_node=(${key//:/ })
             if [[ ${master_node[0]} == ${machine} ]]
             then
                 switch=true
                 node_port=${master_node[1]}
-                slave_node=(${node_map_new[$key]//:/ }) 
+                slave_node=(${node_map_new[$key]//:/ })
+                check_master_status ${master_node[0]} ${master_node[1]}
                 check_slave_status ${slave_node[0]} ${slave_node[1]}
+                if [ "${tmp_machine}"x == "${slave_node[0]}"x ]; then
+                    # 如果机器刚刚重启，保证从节点已经被广播到整个集群
+                    log "${slave_node[0]}:${slave_node[1]} 等待从节点同步到整个集群"
+                    sleep 15
+                fi
                 fail_over ${slave_node[0]} ${slave_node[1]} ${master_node[0]} ${master_node[1]}
-                #log ${key}"-${node_map[$key]}"
+                check_master_status ${slave_node[0]} ${slave_node[1]}
+                check_slave_status ${master_node[0]} ${master_node[1]}
             fi
         done
         log "${machine} 主机待重启，集群信息校验..."
@@ -351,13 +460,16 @@ function restart() {
         fi
         log "${machine} 主机待重启，集群信息校验Ok..."
         restart_machine ${machine} ${node_port}
+        tmp_machine=${machine}
         #sleep 10
         log_c "${machine} 主机重启已完成，集群信息校验..."
         if [ "$switch" = true ]; then
             check_switch ${machine} ${node_port}
-        else 
+        else
             check_switch $o_ip $o_port
         fi
+        check_slave_status ${machine} ${node_port}
+        check_switch ${machine} ${node_port}
         log_c "${machine} 主机已完成，集群信息信息校验Ok..."
         log "已完成机器列表:"
         finish_machines=("${finish_machines[@]}" ${machine})
@@ -381,37 +493,42 @@ function restore() {
 
         # 针对新的信息进行解析
         str_new=(`cat ${path}/cluster_node_2.info | awk 'NR>1 {printf ("%s,%s\n"), $2,$3}'`)
-        for i in ${!str_new[@]} 
+        log ${str_new[*]}
+        for i in ${!str_new[@]}
         do
             # 赋值
             eval $(echo ${str_new[i]} | awk '{split($0, filearray_new, ",");print "node_map_new["filearray_new[1]"]="filearray_new[2]}')
             # eval $(echo ${str_new[i]} | awk '{split($0, filearray_new, ",");print "slave_node_map_new["filearray_new[2]"]="filearray_new[1]}')
-        done 
+        done
 
         log "开始对 "${machine}" 进行修复"
         switch=false
         for key in ${!node_map_new[@]}
-        do  
-            master_node_new=(${key//:/ }) 
+        do
+            master_node_new=(${key//:/ })
             if [[ ${master_node_new[0]} == ${machine} ]]
             then
                 switch=true
                 node_new_port=${master_node_new[1]}
-                slave_node_new=(${node_map_new[$key]//:/ }) 
+                slave_node_new=(${node_map_new[$key]//:/ })
 
                 # 如果新的主节点在原始信息找不到就进行切换
                 if [ ! -n "${node_map[$key]}" ]; then
                     log "准备修复: $key"
+                    check_master_status ${master_node_new[0]} ${master_node_new[1]}
+                    check_slave_status ${slave_node_new[0]} ${slave_node_new[1]}
                     fail_over ${slave_node_new[0]} ${slave_node_new[1]} ${master_node_new[0]} ${master_node_new[1]}
+                    check_master_status ${slave_node_new[0]} ${slave_node_new[1]}
+                    check_slave_status ${master_node_new[0]} ${master_node_new[1]}
                     log "修复完成: $key"
                 fi
             fi
         done
         log_c "${machine} 主机修复已完成，进行集群信息同步和信息校验..."
         if [ "$switch" = true ]; then
-            check_switch ${machine} ${node_new_port[1]}    
+            check_switch ${machine} ${node_new_port}
         else
-            check_switch $o_ip $o_port    
+            check_switch $o_ip $o_port
         fi
         log_c "${machine} 主机修复已完成，集群信息同步和信息校验Ok..."
     done
@@ -433,7 +550,7 @@ function main(){
     cluster_adddress=/${dc_name}/SERVICE/${cluster_name}
     init_path
 
-    if [ "${pwd}"x != x ]; then 
+    if [ "${pwd}"x != x ]; then
         pwd='-a '${pwd}
     fi
 
